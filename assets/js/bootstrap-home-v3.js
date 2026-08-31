507143cb6facf1c52c2ce6432d1ce952534cfb17
poornachand-sekuri <poornarocks@gmail.com>
2026-09-01 02:27:57 +0530

    Polish mobile Home and add Content-style Comments (#21)
    Fix Previously Reviewed poster fitting on mobile Safari, retain the approved smooth Home-page ambience, replicate the Content-page Comments UI and behavior on Home, and tighten the spacing above the Comments section.

--- assets/js/bootstrap-home-v3.js
+++ assets/js/bootstrap-home-v3.js
@@ -4,11 +4,22 @@ const pathParts = url.pathname.split('/').filter(Boolean);
 const isHome = !hasReview && pathParts.length === 0;
 
 if (isHome) {
-  const css = document.createElement('link');
-  css.rel = 'stylesheet';
-  css.href = '/assets/css/home-v3.css?v=20260831-master-v3';
-  document.head.append(css);
-  import('./home-v3.js');
+  const styles = [
+    '/assets/css/home-v3.css?v=20260901-master-v3',
+    '/assets/css/home-mobile-polish.css?v=20260901-home-comments-2',
+    '/assets/css/home-comments.css?v=20260901-home-comments-2'
+  ];
+
+  styles.forEach(href => {
+    const css = document.createElement('link');
+    css.rel = 'stylesheet';
+    css.href = href;
+    document.head.append(css);
+  });
+
+  import('./home-v3.js?v=20260901-home-comments-2')
+    .then(() => import('./home-comments.js?v=20260901-home-comments-2'))
+    .catch(error => console.error('Unable to load the Home page:', error));
 } else {
   import('./app.js').then(async () => {
     const [, , comments] = await Promise.all([
