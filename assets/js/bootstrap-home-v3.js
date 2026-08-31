c612b63f38470311db3a59b403297888ff415af3
poornachand-sekuri <poornarocks@gmail.com>
2026-08-31 23:52:37 +0530

    Route root to fresh Home v3 runtime

---
+++ assets/js/bootstrap-home-v3.js
@@ -0,0 +1,14 @@
+const url = new URL(location.href);
+const hasReview = url.searchParams.has('review');
+const pathParts = url.pathname.split('/').filter(Boolean);
+const isHome = !hasReview && pathParts.length === 0;
+
+if (isHome) {
+  const css = document.createElement('link');
+  css.rel = 'stylesheet';
+  css.href = '/assets/css/home-v3.css?v=20260831-master-v3';
+  document.head.append(css);
+  import('./home-v3.js');
+} else {
+  import('./app.js').then(() => import('./ui-patch.js'));
+}
