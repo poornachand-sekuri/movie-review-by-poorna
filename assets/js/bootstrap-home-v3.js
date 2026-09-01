607f217427e2c9075ef4d32fa9d89ea011c3e9ff
poornachand-sekuri <poornarocks@gmail.com>
2026-09-01 23:46:57 +0530

    Bust cache for continuous Home background

--- assets/js/bootstrap-home-v3.js
+++ assets/js/bootstrap-home-v3.js
@@ -13,7 +13,7 @@ const HOME_ASSETS = [
   '06_share_your_opinion.avif',
   '07_bottom_navigation.avif'
 ];
-const HOME_VERSION = '20260901-lounge-final-3';
+const HOME_VERSION = '20260901-lounge-continuous-bg-4';
 
 if (isHome) {
   [
