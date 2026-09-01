2e79aa54f443abf15243a610dced565425ab4cd6
poornachand-sekuri <poornarocks@gmail.com>
2026-09-01 21:44:10 +0530

    Build Home lounge from v3 AVIF sections

--- assets/js/bootstrap-home-v3.js
+++ assets/js/bootstrap-home-v3.js
@@ -2,33 +2,36 @@ const url = new URL(location.href);
 const hasReview = url.searchParams.has('review');
 const pathParts = url.pathname.split('/').filter(Boolean);
 const isHome = !hasReview && pathParts.length === 0;
-const HOME_MASTER = 'https://assets.moviereviewbypoorna.com/ui/pages/home/v2/mobile/luxury_movie_review_theatre_dashboard_MASTER_LOCKED.avif?v=20260901-master-v3';
+
+const HOME_ASSET_BASE = 'https://assets.moviereviewbypoorna.com/ui/pages/home/v3/mobile';
+const HOME_ASSETS = [
+  '01_background.avif',
+  '02_top_menu_section.avif',
+  '03_now_reviewed_section.avif',
+  '04_recent_reviews_section.avif',
+  '05_previously_reviewed.avif',
+  '06_share_your_opinion.avif',
+  '07_bottom_navigation.avif'
+];
 
 if (isHome) {
-  const styles = [
-    '/assets/css/home-v3.css?v=20260901-master-v3',
-    '/assets/css/home-mobile-polish.css?v=20260901-home-comments-2',
-    '/assets/css/home-comments.css?v=20260901-home-comments-2'
-  ];
+  const css = document.createElement('link');
+  css.rel = 'stylesheet';
+  css.href = '/assets/css/home-v3.css?v=20260901-lounge-redesign-1';
+  document.head.append(css);
 
-  styles.forEach(href => {
-    const css = document.createElement('link');
-    css.rel = 'stylesheet';
-    css.href = href;
-    document.head.append(css);
+  // Preload the background and the first two visible shells. The remaining
+  // authored section images are discovered immediately by home-v3.js.
+  HOME_ASSETS.slice(0, 3).forEach(file => {
+    const preload = document.createElement('link');
+    preload.rel = 'preload';
+    preload.as = 'image';
+    preload.href = `${HOME_ASSET_BASE}/${file}?v=20260901-lounge-redesign-1`;
+    document.head.append(preload);
   });
 
-  // Discover the large Home artwork early, but let the browser balance it
-  // against critical CSS and the compact review catalogue on slower networks.
-  const masterPreload = document.createElement('link');
-  masterPreload.rel = 'preload';
-  masterPreload.as = 'image';
-  masterPreload.href = HOME_MASTER;
-  document.head.append(masterPreload);
-
   Promise.all([
-    import('./home-v3.js?v=20260901-home-comments-2')
-      .then(() => import('./home-comments.js?v=20260901-home-comments-2')),
+    import('./home-v3.js?v=20260901-lounge-redesign-1'),
     import('./analytics.js')
   ]).catch(error => console.error('Unable to load the Home page:', error));
 } else {
