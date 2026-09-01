1d3d5147409835243a13d1464a1b9e8485a2cc09
poornachand-sekuri <poornarocks@gmail.com>
2026-09-02 02:31:05 +0530

    Bust cache for expandable live Home sections

--- assets/js/bootstrap-home-v3.js
+++ assets/js/bootstrap-home-v3.js
@@ -13,7 +13,7 @@ const HOME_ASSETS = [
   '06_share_your_opinion.avif',
   '07_bottom_navigation.avif'
 ];
-const HOME_VERSION = '20260902-now-popup-taller-9';
+const HOME_VERSION = '20260902-expandable-live-sections-10';
 
 if (isHome) {
   [
