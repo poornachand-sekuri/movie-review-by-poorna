f2c0b864ff12e454c8daa7814ae804eaec40590f
poornachand-sekuri <poornarocks@gmail.com>
2026-09-02 02:16:17 +0530

    Bust cache for responsive POV and ticket popup

--- assets/js/bootstrap-home-v3.js
+++ assets/js/bootstrap-home-v3.js
@@ -13,7 +13,7 @@ const HOME_ASSETS = [
   '06_share_your_opinion.avif',
   '07_bottom_navigation.avif'
 ];
-const HOME_VERSION = '20260902-now-title-left-padding-7';
+const HOME_VERSION = '20260902-now-popup-pov-autofit-8';
 
 if (isHome) {
   [
