b86309499f1660fe237e0a00705a566efddc87b9
poornachand-sekuri <poornarocks@gmail.com>
2026-09-01 12:21:46 +0530

    Preload Home master artwork early

--- assets/js/bootstrap-home-v3.js
+++ assets/js/bootstrap-home-v3.js
@@ -2,8 +2,16 @@ const url = new URL(location.href);
 const hasReview = url.searchParams.has('review');
 const pathParts = url.pathname.split('/').filter(Boolean);
 const isHome = !hasReview && pathParts.length === 0;
+const HOME_MASTER = 'https://assets.moviereviewbypoorna.com/ui/pages/home/v2/mobile/luxury_movie_review_theatre_dashboard_MASTER_LOCKED.avif?v=20260901-master-v3';
 
 if (isHome) {
+  const masterPreload = document.createElement('link');
+  masterPreload.rel = 'preload';
+  masterPreload.as = 'image';
+  masterPreload.href = HOME_MASTER;
+  masterPreload.fetchPriority = 'high';
+  document.head.append(masterPreload);
+
   const styles = [
     '/assets/css/home-v3.css?v=20260901-master-v3',
     '/assets/css/home-mobile-polish.css?v=20260901-home-comments-2',
