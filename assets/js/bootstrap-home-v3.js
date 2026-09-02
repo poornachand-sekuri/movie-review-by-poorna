38d258d1b251fcbafc19c45f6a2122b87a5fab5c
poornachand-sekuri <poornarocks@gmail.com>
2026-09-02 18:35:24 +0530

    Bump Home stable text runtime asset version

--- assets/js/bootstrap-home-v3.js
+++ assets/js/bootstrap-home-v3.js
@@ -13,7 +13,7 @@ const HOME_ASSETS = [
   '06_share_your_opinion.avif',
   '07_bottom_navigation.avif'
 ];
-const HOME_VERSION = '20260902-home-title-pov-21';
+const HOME_VERSION = '20260902-home-stable-text-22';
 const CONTENT_VERSION = '20260902-content-polish-17';
 
 if (isHome) {
